e40480ec441147b83c66dce67c675686d6f6bcf1
xandre9 <xandre9@xandre9.xandre9>
2022-05-01 00:41:26 -0400

    Modified footer, validated and formatted code.

--- src/index.js
+++ src/index.js
@@ -11,12 +11,9 @@ function restaurantName() {
 function navBar() {
   const nav = document.createElement('nav');
   nav.classList = "nav-bar";
-
   const ul = document.createElement('ul');
   ul.classList = 'ul-area';
-
   nav.appendChild(restaurantName());
-
   const menuListItem = document.createElement('li');
   const menuAnchor = document.createElement('a');
   menuListItem.appendChild(menuAnchor);
@@ -24,7 +21,6 @@ function navBar() {
   menuAnchor.classList = "nav-anchor-link";
   menuAnchor.setAttribute('href', 'https://www.google.com')
   ul.appendChild(menuListItem);
-
   const locationListItem = document.createElement('li');
   const locationAnchor = document.createElement('a');
   locationListItem.appendChild(locationAnchor);
@@ -32,9 +28,7 @@ function navBar() {
   locationAnchor.classList = "nav-anchor-link";
   locationAnchor.setAttribute('href', 'https://www.google.com')
   ul.appendChild(locationListItem);
-
   nav.appendChild(ul);
-
   return nav;
 }
 
@@ -44,46 +38,31 @@ whet.classList = 'splash-image';
 console.log(whet);
 
 const restaurantDescription = () => {
-
   const br = document.createElement('br');
-
   const pitchDiv = document.createElement('div');
   const strong = document.createElement('strong');
-  
   const p2 = document.createElement('p');
   const p3 = document.createElement('p');
   const p4 = document.createElement('p');
-
-
-
   strong.textContent = 'The Finest Foods in the World At Your Selection.';
   p2.textContent = 'At The Selection you gain access to the creme de la creme.';
   p3.textContent = 'Our chefs are world renowned and are passionate about providing our guests with the best dining experience in the world.';
   p4.textContent = 'Indulge yourself at our lovely locations that are culturally inspired with exclusive art.';
-
   pitchDiv.appendChild(strong);
   pitchDiv.appendChild(br);
   pitchDiv.appendChild(p2);
   pitchDiv.appendChild(p3);
   pitchDiv.appendChild(p4);
-
-
   return pitchDiv;
-
 }
-
-
 
 function footer() {
   const footer = document.createElement('footer');
-  footer.textContent = 'xandre9';
+  footer.textContent = 'Designed by Xandre9';
   return footer
 }
 
 const content = document.getElementById('content');
-// console.log(content);
-
-
 content.appendChild(navBar());
 content.appendChild(whet);
 content.appendChild(restaurantDescription());
