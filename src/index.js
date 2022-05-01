49f37a26523e57cba55651f49b95d53f887e0f88
xandre9 <xandre9@xandre9.xandre9>
2022-05-01 00:12:59 -0400

    Changed first  element to  element.

--- src/index.js
+++ src/index.js
@@ -45,26 +45,29 @@ console.log(whet);
 
 const restaurantDescription = () => {
 
+  const br = document.createElement('br');
+
   const pitchDiv = document.createElement('div');
-  const p1 = document.createElement('p');
+  const strong = document.createElement('strong');
+  
   const p2 = document.createElement('p');
   const p3 = document.createElement('p');
   const p4 = document.createElement('p');
 
-  p1.textContent = 'The finest foods in the world, at your selection.';
+
+
+  strong.textContent = 'The Finest Foods in the World At Your Selection.';
   p2.textContent = 'At The Selection you gain access to the creme de la creme.';
   p3.textContent = 'Our chefs are world renowned and are passionate about providing our guests with the best dining experience in the world.';
   p4.textContent = 'Indulge yourself at our lovely locations that are culturally inspired with exclusive art.';
 
-  pitchDiv.appendChild(p1);
+  pitchDiv.appendChild(strong);
+  pitchDiv.appendChild(br);
   pitchDiv.appendChild(p2);
   pitchDiv.appendChild(p3);
   pitchDiv.appendChild(p4);
 
-{/* <div id="pitch">At The Selection you gain access to the creme de la creme. <p>Our chefs are world renowned and are
-  passionate about providing our guests with the best dining experience in the world. <p>Indulge yourself at our
-  lovely locations that are culturally inspired with exclusive art.</p></div>
-<br></br> */}
+
   return pitchDiv;
 
 }
@@ -86,14 +89,6 @@ content.appendChild(whet);
 content.appendChild(restaurantDescription());
 content.appendChild(footer());
 
-
-
-{/* <p>The finest foods in the world, at your selection.</p>
-
-<div id="pitch">At The Selection you gain access to the creme de la creme. <p>Our chefs are world renowned and are
-  passionate about providing our guests with the best dining experience in the world. <p>Indulge yourself at our
-  lovely locations that are culturally inspired with exclusive art.</p></div>
-<br> */}
 
 
 
