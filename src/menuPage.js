601605aaa12bb7597a8966bf30ab20c7f1b7491b
xandre9-git <jmjames673@gmail.com>
2023-01-05 20:00:34 -0500

    Testing menuPage() and found that it executes

--- src/menuPage.js
+++ src/menuPage.js
@@ -2,12 +2,13 @@ import frontPage from "./frontPage";
 
 
 export default function menuPage(){
+  // body.removeChild(content);
   const divMenu = document.createElement('div');
   divMenu.classList = 'food-drinks-menu';
   const foodTitle = document.createElement('p');
   foodTitle.textContent ="NAME OF ENTREE";
   divMenu.appendChild(foodTitle);
   const content = document.getElementById('content');
-  
+  console.log('Firing.')
   content.appendChild(divMenu);
 }
