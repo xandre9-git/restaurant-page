c6cea0467107d00eb43cf75208b5781739767655
xandre9-git <jmjames673@gmail.com>
2023-01-05 21:33:59 -0500

    Remove unnecessary code

--- src/menuPage.js
+++ src/menuPage.js
@@ -2,7 +2,9 @@ import frontPage from "./frontPage";
 
 
 export default function menuPage(){
-  // body.removeChild(content);
+  // this needs to clear body of page
+  
+
   const divMenu = document.createElement('div');
   divMenu.classList = 'food-drinks-menu';
   const foodTitle = document.createElement('p');
