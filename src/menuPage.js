183ba6fb6d5b83ac4b4691f1f41a21de2ac54a32
xandre9-git <jmjames673@gmail.com>
2023-01-07 14:47:31 -0500

    Change id name of objects to class, add Entrees title for list of entrees, create div for unique entree and give it a class of entree, and add a div for a description of the entree's sides.

--- src/menuPage.js
+++ src/menuPage.js
@@ -4,22 +4,22 @@ import { footer } from "./pageDefaults.js";
 
 const menu = [
   {
-    id: "wagyu",
-    entree: "8 Ounce Wagyu Steak",
-    sides: "Garlic Mashed Potatoes and Asparagus", 
+    class: "entree",
+    entree: "8 OUNCE WAGYU STEAK",
+    sides: "GARLIC MASHED POTATOES AND ASPARAGUS", 
   },
   {
-    id: "salmon",
+    class: "entree",
     entree: "Alaskan Salmon",
     sides: "Wild Rice and Spinach"
   },
   {
-    id: "chicken",
+    class: "entree",
     entree: "Honey Barbecued Chicken Breast",
     sides: "Whole Wheat Couscous and Chopped Cabbage"
   },
   {
-    id: "salad",
+    class: "entree",
     entree: "Roasted Sweet Potato Salad",
     sides: "Kale, Avocadoes, and Crushed Pecans"
   }
@@ -36,11 +36,20 @@ export default function menuPage(){
   divMenu.classList = 'food-drinks-menu';
   const entrees = document.createElement('div');
   entrees.classList = "entrees";
+  const entreesTitle = document.createElement('h1');
+  entreesTitle.textContent = "ENTREES"
+  entrees.appendChild(entreesTitle);
   divMenu.appendChild(entrees)
-  const entree = "";
-  const selectionTitle = document.createElement('h2');
-  selectionTitle.textContent = menu[0].entree;
-  entrees.appendChild(selectionTitle);
+  const entree = document.createElement('div');
+  entree.classList = menu[0].class;
+  entrees.appendChild(entree)
+  const entreeTitle = document.createElement('h2');
+  entreeTitle.textContent = menu[0].entree;
+  const entreeSide = document.createElement('div');
+  entreeSide.classList = "entree-desc";
+  entreeSide.textContent = menu[0].sides;
+  entree.appendChild(entreeTitle);
+  entree.appendChild(entreeSide)
   const content = document.getElementById('content');
   console.log('Firing.')
   content.appendChild(divMenu);
