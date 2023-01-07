007c66a8d6f9910c65c4b1536ef7831175d129f0
xandre9-git <jmjames673@gmail.com>
2023-01-07 14:04:44 -0500

    Pull footer function out of page defaults function for use in other areas of program

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,18 +1,48 @@
 import frontPage from "./frontPage";
+import pageDefaults from "./pageDefaults.js";
+import { footer } from "./pageDefaults.js";
 
+const menu = [
+  {
+    id: "wagyu",
+    entree: "8 Ounce Wagyu Steak",
+    sides: "Garlic Mashed Potatoes and Asparagus", 
+  },
+  {
+    id: "salmon",
+    entree: "Alaskan Salmon",
+    sides: "Wild Rice and Spinach"
+  },
+  {
+    id: "chicken",
+    entree: "Honey Barbecued Chicken Breast",
+    sides: "Whole Wheat Couscous and Chopped Cabbage"
+  },
+  {
+    id: "salad",
+    entree: "Roasted Sweet Potato Salad",
+    sides: "Kale, Avocadoes, and Crushed Pecans"
+  }
+]
+  
 
 export default function menuPage(){
-  // this needs to clear body of page
   const bodyDoc = document.getElementsByClassName('body-content');
   console.log(bodyDoc);
   bodyDoc[0].remove();
-
+  const footer = document.getElementById('footer');
+  footer.remove();
   const divMenu = document.createElement('div');
   divMenu.classList = 'food-drinks-menu';
-  const foodTitle = document.createElement('p');
-  foodTitle.textContent ="NAME OF ENTREE";
-  divMenu.appendChild(foodTitle);
+  const entrees = document.createElement('div');
+  entrees.classList = "entrees";
+  divMenu.appendChild(entrees)
+  const entree = "";
+  const selectionTitle = document.createElement('h2');
+  selectionTitle.textContent = menu[0].entree;
+  entrees.appendChild(selectionTitle);
   const content = document.getElementById('content');
   console.log('Firing.')
   content.appendChild(divMenu);
+  content.appendChild(footer);
 }
