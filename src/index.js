c179d841c9441cffd4a68599057240a7d2df63dc
xandre9-git <jmjames673@gmail.com>
2023-01-11 20:51:13 -0500

    Add event listener for click of 'The Selection' anchor link

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import menuPage from "./menuPage";
 import { pageDefaults } from "./pageDefaults.js";
 
 pageDefaults();
+
+const splashPage = document.getElementsByClassName("nav-anchor-link")[0];
+console.log(splashPage);
+splashPage.addEventListener("click", () => {
+  frontPage();
+});
