dbf1eef1c8c8fdd930cb96214555f9588d025234
xandre9-git <jmjames673@gmail.com>
2023-01-11 20:52:12 -0500

    Add descriptive title comments for functions in the script

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import frontPage from "./frontPage";
 import menuPage from "./menuPage";
 import { pageDefaults } from "./pageDefaults.js";
 
+// Landing Page Function
 pageDefaults();
 
+// Front Page Add Event Listener
 const splashPage = document.getElementsByClassName("nav-anchor-link")[0];
-console.log(splashPage);
 splashPage.addEventListener("click", () => {
   frontPage();
 });
+
+// Menu Page Add Event Listener
+
