9d9c58a4e47ede1d97ae8a8bd17a601ceea1c260
xandre9-git <jmjames673@gmail.com>
2023-01-13 13:30:17 -0500

    Add Menu Page and Contact Page click event listeners

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import "./styles.css";
 import frontPage from "./frontPage";
 import menuPage from "./menuPage";
+import { contactPage } from "./contactPage";
 import { pageDefaults } from "./pageDefaults.js";
 
 // Landing Page Function
 pageDefaults();
 
 // Front Page Add Event Listener
-const splashPage = document.getElementsByClassName("nav-anchor-link")[0];
+const splashPage = document.getElementById("the-selection-link");
 splashPage.addEventListener("click", () => {
   frontPage();
 });
 
 // Menu Page Add Event Listener
+const menuList = document.getElementById("menu-link");
+menuList.addEventListener("click", menuPage);
 
+// Contact Page Add Event Listener
+const contactInfo = document.getElementById("contact-link");
+console.log(contactInfo);
+contactInfo.addEventListener("click", contactPage);
