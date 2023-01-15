a5b2bdef8154e5cece27c731dea19a7f4fcf2d49
xandre9-git <jmjames673@gmail.com>
2023-01-14 22:27:14 -0500

    Add more font types to test section of rules since Wilmina font requires it

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         type: 'asset/resource',
       },
       {
-        test: /\.(woff|woff2)$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
